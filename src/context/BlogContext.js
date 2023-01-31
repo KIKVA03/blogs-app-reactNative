83df4abace1e75839197805f020816e9b271e26b
KIKVA03 <datokikvadze8@gmail.com>
2023-01-31 21:44:10 +0400

    feat seccond commit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useReducer } from "react";
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+    switch (action.type) {
+        case "ADD":
+            return [...state, { title: `blogPost #${state.length + 1}` }];
+        default:
+            return state;
+    }
+};
 export const BlogProvaider = ({ children }) => {
-    // const blogPosts = [{ title: "blog post #1" }, { title: "blog post #2" }];
-    const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, dispatch] = useReducer(blogReducer, []);
+    // const [blogPosts, setBlogPosts] = useState([]);
     const addBlockPost = () => {
-        setBlogPosts([...blogPosts, { title: `blog post #${blogPosts.length + 1}` }]);
+        dispatch({ type: "ADD" });
     };
     return (
         <BlogContext.Provider value={{ data: blogPosts, addBlockPost }}>
